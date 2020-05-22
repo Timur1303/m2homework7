404fadba902e1025acdab6fa0b7f90e2172158ae
Timur1303 <tima130394@gmail.com>
2020-05-23 01:48:00 +0600

    v2

--- src/Main.java
+++ src/Main.java
@@ -24,21 +24,32 @@ public class Main {
             System.out.println("Введите: ");
             listB.add(scanner.nextLine());
         }
-        iteratorB = listB.iterator();
-        while (iteratorB.hasNext()) {
-            System.out.println(iteratorB.next());
-        }
+            iteratorB = listB.iterator();
+            while (iteratorB.hasNext()) {
+                System.out.println(iteratorB.next());
+            }
 
-        ArrayList<String> listC = new ArrayList<>(listA.size() + listB.size());
-        Collections.reverse(listB);
-        listC.addAll(listA);
-        listC.addAll(listB);
+
+        ArrayList<String> listC = new ArrayList<>();
+        /*listC.addAll(listA);
+        listC.add(1,listB.get(4));
+        listC.add(3,listB.get(3));
+        listC.add(5,listB.get(2));
+        listC.add(7,listB.get(1));
+        listC.add(9,listB.get(0));*/
+//        Collections.reverse(listB);
+        while (iteratorA.hasNext() && iteratorB.hasNext()) {
+            listC.add(iteratorA.next());
+            listC.add(iteratorB.next());
+        }
 
         System.out.println(listC);
         System.out.println("--------------------");
 
+
         Collections.sort(listC);
         System.out.println(listC);
+
 
 
     }
